58eb6820d15c22f35168f4a7741d8aafe62addaf
remidbs <remi.delbouys@laposte.net>
2017-01-07 10:09:57 +0100

    implemented classfication like in paper 1

--- classifier.py
+++ classifier.py
@@ -1,11 +1,42 @@
 import pandas as pd
 import numpy as np
 from sklearn import svm
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, train_test_split
 
 DF = pd.read_csv("features/features30-12.csv", index_col=None, header=None)
 DF = DF.rename(columns={len(DF.columns)-1 : "label",len(DF.columns)-2:"name"})
-classifier = svm.SVC()
+
+DF_forward = DF[DF.label]
+F_X_AB, F_X_C, F_y_AB, F_y_C = train_test_split(DF_forward.drop(["name","label"], axis=1),DF_forward.label, test_size = 1.0/3)
+F_X_A, F_X_B, F_y_A, F_y_B = train_test_split(F_X_AB, F_y_AB, test_size= 0.5 )
+
+DF_backward = DF[DF.label == False]
+B_X_CB, B_X_A, B_y_CB, B_y_A = train_test_split(DF_backward.drop(["name","label"], axis=1),DF_backward.label, test_size = 1.0/3)
+B_X_C, B_X_B, B_y_C, B_y_B = train_test_split(B_X_CB, B_y_CB, test_size= 0.5 )
+
+X_A = pd.concat([B_X_A,F_X_A], axis=0)
+y_A = pd.concat([B_y_A,F_y_A], axis=0)
+X_B = pd.concat([B_X_B,F_X_B], axis=0)
+y_B = pd.concat([B_y_B,F_y_B], axis=0)
+X_C = pd.concat([B_X_C,F_X_C], axis=0)
+y_C = pd.concat([B_y_C,F_y_C], axis=0)
+
+for C in [0.01,0.1,1.0,10.0,100.0,1000.0,10000.0]:
+    print "C:",C
+    for name,X_1,y_1,X_2,y_2,X_3,y_3 in [("C",X_A,y_A,X_B,y_B,X_C,y_C),
+                                         ("A",X_B,y_B,X_C,y_C,X_A,y_A),
+                                         ("B",X_C,y_C,X_A,y_A,X_B,y_B)]:
+        print "Testing on",name,". Training on the 2 other folds."
+        X_train = pd.concat([X_1,X_2],axis=0)
+        y_train = pd.concat([y_1,y_2],axis=0)
+        X_test = X_3
+        y_test = y_3
+        classifier = svm.SVC(C, class_weight={True:0.16502,False:1})
+        classifier.fit(X_train, y_train)
+        pred = classifier.predict(X_test)
+        print np.mean(1-np.abs(pred-y_test))
+    
+
 
 n_splits = 4
 scores = cross_val_score(classifier, DF.drop(["name","label"], axis=1), DF.label, cv=4)
